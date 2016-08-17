c7cc7067033875fe34ffae7662739e07a29d7afd
Mike <mikekh@uw.edu>
2016-08-16 19:04:33 -0700

    Added pop, size, and search

--- src/linked_list.py
+++ src/linked_list.py
@@ -1,26 +1,49 @@
+
 class Node(object):
     """Initialize node object."""
+
     def __init__(self, data, next=None):
+        """Initialize with dat and next."""
         self.data = data
         self.next = next
 
 
 class LinkedList(object):
     """Initialize linklist object."""
-    def __init__(self):
-        self.head = None
-        self.size = 0
 
+    def __init__(self):
+        """Initialize with head and count."""
+        self.head = None
+        self.count = 0
 
     def push(self, data):
         """Push value to linked list."""
-        if self.head == None:
-            node = Node(data)
+        node = Node(data)
+        if self.head is None:
             self.head = node
-            self.size += 1
+            self.count += 1
         else:
-            node = Node(data)
-            node2 = self.head
-            node2.next = node
+            node.next = self.head
             self.head = node
-            self.size += 1
+            self.count += 1
+
+    def size(self):
+        """Return size of linked list."""
+        return self.count
+
+    def search(self, data):
+        """Search for data and return node."""
+        node = self.head
+        while node is not None and node.data != data:
+            node = node.next
+        return node
+
+    def pop(self):
+        """Return value of head of the linked list."""
+        if self.head is not None:
+            pop = self.head
+            self.head = self.head.next
+            self.count -= 1
+            return pop.data
+        else:
+            raise IndexError
--- src/test_linked_list.py
+++ src/test_linked_list.py
@@ -8,8 +8,9 @@ TEST_TABLE = [
     ([], [])
 ]
 
+
 def test_node_value():
-    """Tests to see if node has a value."""
+    """Test to see if node has a value."""
     from linked_list import Node
     test_node = Node(5)
     assert test_node.data == 5
@@ -24,52 +25,161 @@ def test_node_next():
 
 @pytest.mark.parametrize('n, result', TEST_TABLE)
 def test_node_value_table(n, result):
-    """Tests node using table."""
+    """Test node using table."""
     from linked_list import Node
     test_node = Node(n)
     assert test_node.data == result
 
 
 def test_linked_list():
-    """Tests that the initial size is 0"""
+    """Test that the initial size is 0."""
     from linked_list import LinkedList
-    test_LinkedList = LinkedList()
-    assert test_LinkedList.size == 0
+    test_ll = LinkedList()
+    assert test_ll.count == 0
 
 
 def test_ll_head():
+    """Test link list head is none."""
     from linked_list import LinkedList
-    test_LinkedList = LinkedList()
-    assert test_LinkedList.head is None
+    test_ll = LinkedList()
+    assert test_ll.head is None
 
 
 def test_ll_push():
-    """Tests push method on linked list"""
+    """Test push method on linked list."""
     from linked_list import LinkedList
-    test_LinkedList = LinkedList()
-    test_LinkedList.push(3)
-    assert test_LinkedList.size == 1
+    test_ll = LinkedList()
+    test_ll.push(3)
+    assert test_ll.count == 1
+
 
 def test_ll_push2():
     """Test value of linked list head."""
     from linked_list import LinkedList
-    test_LinkedList = LinkedList()
-    test_LinkedList.push(2)
-    assert test_LinkedList.head.data == 2
+    test_ll = LinkedList()
+    test_ll.push(2)
+    assert test_ll.head.data == 2
+
 
 def test_push_nodes():
-    """Test size of pushed linked list"""
+    """Test size of pushed linked list."""
     from linked_list import LinkedList
-    test_LinkedList = LinkedList()
-    test_LinkedList.push(1)
-    test_LinkedList.push(2)
-    assert test_LinkedList.size == 2
+    test_ll = LinkedList()
+    test_ll.push(1)
+    test_ll.push(2)
+    assert test_ll.count == 2
 
 
 def test_push_head_data():
     """Insert into linked list and see which is the head."""
     from linked_list import LinkedList
-    test_LinkedList = LinkedList()
-    test_LinkedList.push(1)
-    test_LinkedList.push(2)
-    assert test_LinkedList.head.data == 2
+    test_ll = LinkedList()
+    test_ll.push(1)
+    test_ll.push(2)
+    assert test_ll.head.data == 2
+
+
+def test_size_of_1():
+    """Test size of Linked_list size is 1."""
+    from linked_list import LinkedList
+    test_ll = LinkedList()
+    test_ll.push(1)
+    assert test_ll.size() == 1
+
+
+def test_size_of_5():
+    """Test size of Linked List size 5."""
+    from linked_list import LinkedList
+    test_ll = LinkedList()
+    for i in range(5):
+        test_ll.push(i)
+    assert test_ll.size() == 5
+
+
+def test_search():
+    """Search ll for a given data and return node."""
+    from linked_list import LinkedList
+    test_ll = LinkedList()
+    test_ll.push(1)
+    assert test_ll.search(1).data == 1
+
+
+def test_search_none():
+    """Search ll for data not there and return none."""
+    from linked_list import LinkedList
+    test_ll = LinkedList()
+    for i in range(5):
+        test_ll.push(i)
+    assert test_ll.search(10) is None
+
+
+def test_search_data_middle():
+    """Search ll data in middle of ll."""
+    from linked_list import LinkedList
+    test_ll = LinkedList()
+    for i in range(5):
+        test_ll.push(i)
+    assert test_ll.search(3).data == 3
+
+
+def test_pop():
+    """Test pop."""
+    from linked_list import LinkedList
+    test_ll = LinkedList()
+    test_ll.push(1)
+    assert test_ll.pop() == 1
+
+
+def test_pop_long():
+    """Test pop with longer linked list."""
+    from linked_list import LinkedList
+    test_ll = LinkedList()
+    for i in range(8):
+        test_ll.push(i)
+    assert test_ll.pop() == 7
+
+
+def test_pop_size_1():
+    """Test pop."""
+    from linked_list import LinkedList
+    test_ll = LinkedList()
+    test_ll.push(1)
+    test_ll.pop()
+    assert test_ll.size() == 0
+
+
+def test_pop_size_long():
+    """Test pop with longer linked list."""
+    from linked_list import LinkedList
+    test_ll = LinkedList()
+    for i in range(8):
+        test_ll.push(i)
+    test_ll.pop()
+    assert test_ll.size() == 7
+
+
+def test_pop_size_rehead():
+    """Test pop."""
+    from linked_list import LinkedList
+    test_ll = LinkedList()
+    test_ll.push(1)
+    test_ll.pop()
+    assert test_ll.head is None
+
+
+def test_pop_rehead_long():
+    """Test pop with longer linked list."""
+    from linked_list import LinkedList
+    test_ll = LinkedList()
+    for i in range(8):
+        test_ll.push(i)
+    test_ll.pop()
+    assert test_ll.head.data == 6
+
+
+def test_pop_empty_ll():
+    """Test pop on empty list."""
+    from linked_list import LinkedList
+    test_ll = LinkedList()
+    with pytest.raises(IndexError):
+        test_ll.pop()
