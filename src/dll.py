6969b82b326e755e379014b3c8cd121bbc433cba
Mike <mikekh@uw.edu>
2016-08-18 16:06:57 -0700

    Added tests for pop and method for pop.

--- src/dll.py
+++ src/dll.py
@@ -1,8 +1,8 @@
 class Node(object):
-    """Node class"""
+    """Node class."""
 
     def __init__(self, data, prev=None, next=None):
-        """Initializes Node."""
+        """Initialize Node."""
         self.data = data
         self.prev = prev
         self.next = next
@@ -12,6 +12,7 @@ class DoublyLinkedList(object):
     """Doubly linked list class."""
 
     def __init__(self, iterable=None):
+        """Intialize DLL."""
         self.head = None
         self.tail = None
         self._size = 0
@@ -20,7 +21,7 @@ class DoublyLinkedList(object):
                 self.push(item)
 
     def push(self, data):
-        """Pushes data into doubly linked list."""
+        """Push data into doubly linked list."""
         node = Node(data)
         if self.head is None:
             self.head = node
@@ -32,7 +33,7 @@ class DoublyLinkedList(object):
         self._size += 1
 
     def append(self, data):
-        """Adds data to the tail of the doubly linked list"""
+        """Add data to the tail of the doubly linked list."""
         node = Node(data)
         if self.tail is None:
             self.head = node
@@ -42,3 +43,14 @@ class DoublyLinkedList(object):
             node.prev = self.tail
             self.tail = node
         self._size += 1
+
+    def pop(self):
+        """Return value the value of the head."""
+        if self.head is not None:
+            pop = self.head
+            self.head = self.head.next
+            self.head.prev = None
+            self._size -= 1
+            return pop.data
+        else:
+            raise IndexError("Doubly Linked List is empty.")
