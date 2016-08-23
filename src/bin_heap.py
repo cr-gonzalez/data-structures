83c3c5017eb56c985aec281e690e622c29d349b7
clessor88 <clessor88@yahoo.com>
2016-08-23 16:28:47 -0700

    created push function for heap, and 4 tests

---
+++ src/bin_heap.py
@@ -0,0 +1,20 @@
+class BinHeap(object):
+    """Class created for binary heap."""
+
+    def __init__(self, iterable=None):
+        """Initialize heap."""
+        self._heap = []
+        if iterable is not None:
+            for item in iterable:
+                self.push(item)
+
+    def push(self, data):
+        """Add value to heap."""
+        self._heap.append(data)
+        i = len(self._heap) - 1
+        parent = (i - 1) // 2
+        while self._heap[i] > self._heap[parent]:
+            self._heap[i], self._heap[parent] = self._heap[parent], self._heap[i]
+            i = parent
+            parent = (i -1) // 2
+            
---
+++ src/test_bin_heap.py
@@ -0,0 +1,34 @@
+import pytest
+
+
+def test_push():
+    """Tests that initial push is the root."""
+    from bin_heap import BinHeap
+    test_bh = BinHeap()
+    test_bh.push(15)
+    assert test_bh._heap[0] == 15
+
+
+def test_push_on_one():
+    """Push smaller value to max heap and check if its a child of the root."""
+    from bin_heap import BinHeap
+    test_bh = BinHeap()
+    test_bh.push(50)
+    test_bh.push(12)
+    assert test_bh._heap[1] == 12
+
+
+def test_push_bigger():
+    """Push larger value to max heap and check if root changed."""
+    from bin_heap import BinHeap
+    test_bh = BinHeap()
+    test_bh.push(50)
+    test_bh.push(55)
+    assert test_bh._heap[0] == 55
+
+
+def test_push_bunches():
+    from bin_heap import BinHeap
+    test_bh = BinHeap([20, 15, 12, 10, 8])
+    test_bh.push(22)
+    assert test_bh._heap[5] == 22
