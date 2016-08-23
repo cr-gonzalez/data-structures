69f439d45295488d29d3b40b94ecddd8faf7590f
Mike <mikekh@uw.edu>
2016-08-22 20:15:04 -0700

    Tests and functions for queue.

--- src/deque.py
+++ src/deque.py
@@ -15,3 +15,29 @@ class Deque(object):
     def appendleft(self, data):
         """Append data to start of Deque."""
         self._DLL.push(data)
+
+    def pop(self):
+        """Pop value at end then return."""
+        return self._DLL.shift()
+
+    def popleft(self):
+        """Pop value at begining and then return."""
+        return self._DLL.pop()
+
+    def peek(self):
+        """Return value at end without removing."""
+        try:
+            return self._DLL.tail.data
+        except AttributeError:
+            return None
+
+    def peekleft(self):
+        """Return value at begining without removing."""
+        try:
+            return self._DLL.head.data
+        except:
+            return None
+
+    def size(self):
+        """Return size of deque."""
+        return self._DLL._size
