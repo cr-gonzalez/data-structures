10765df871502c269b3a5c85cbf0e77c63c8abab
Mike <mikekh@uw.edu>
2016-08-29 15:17:09 -0700

    Added first bit of functionality.

--- src/simple_graph.py
+++ src/simple_graph.py
@@ -11,3 +11,22 @@ class SimpleGraph(object):
             raise KeyError("Node is already in graph")
         else:
             self._graph[node] = []
+
+    def add_edge(self, node1, node2):
+        """Add an edge to a node."""
+        if node1 not in self._graph:
+            self.add_node(node1)
+        elif node2 not in self._graph:
+            self.add_node(node2)
+        if node2 not in self._graph[node1]:
+            self._graph[node1].append(node2)
+
+    def del_node(self, node):
+        """Delete a node and references to it."""
+        if node not in self._graph:
+            raise KeyError
+        else:
+            self._graph.pop(node)
+            for key in self._graph:
+                if node in self._graph[key]:
+                    self._graph[key].remove(node)
--- src/test_simple_graph.py
+++ src/test_simple_graph.py
@@ -1,18 +1,80 @@
 import pytest
 
 
-def test_add_node():
+def test_add_node(graph_empty):
     """Test to check adding a node."""
-    from simple_graph import SimpleGraph
-    test_sg = SimpleGraph()
-    test_sg.add_node('a')
-    assert 'a' in test_sg._graph
+    graph_empty.add_node('a')
+    assert 'a' in graph_empty._graph
 
 
-def test_add_node_2():
+def test_add_node_2(graph_one):
     """Test to check adding more then 1 node."""
-    from simple_graph import SimpleGraph
-    test_sg = SimpleGraph()
-    test_sg.add_node('a')
-    test_sg.add_node('b')
-    assert test_sg._graph == {'a': [], 'b': []}
+    graph_one.add_node('b')
+    assert graph_one._graph == {'a': [], 'b': []}
+
+
+def test_add_edge_to_one(graph_one):
+    """Test to check adding an edge."""
+    graph_one.add_edge('a', 'b')
+    assert 'b' in graph_one._graph['a']
+
+
+def test_add_edge_to_empty(graph_empty):
+    """Test that it adds nodes and adds edge."""
+    graph_empty.add_edge('z', 'y')
+    assert 'y' in graph_empty._graph['z']
+
+
+def test_add_2_edges(graph_one):
+    """Add more than one edge."""
+    graph_one.add_edge('a', 'g')
+    graph_one.add_edge('a', 'r')
+    assert graph_one._graph['a'] == ['g', 'r']
+
+
+def test_add_same_edge(graph_two):
+    """Add the same edge twice."""
+    graph_two.add_edge('a', 'z')
+    graph_two.add_edge('a', 'z')
+    assert graph_two._graph['a'] == ['z']
+
+
+def test_del_node_empty(graph_empty):
+    """Test del on empty."""
+    with pytest.raises(KeyError):
+        graph_empty.del_node('a')
+
+
+def test_del_node_one(graph_one):
+    """Test del on one."""
+    graph_one.del_node('a')
+    assert 'a' not in graph_one._graph
+
+
+def test_del_node_key(graph_two):
+    """Test del node is not a key."""
+    graph_two.add_edge('a', 'z')
+    graph_two.add_edge('a', 'f')
+    graph_two.del_node('f')
+    assert 'f' not in graph_two._graph
+
+
+def test_del_node_in_a(graph_two):
+    """Test del is not in a's val."""
+    graph_two.add_edge('a', 'z')
+    graph_two.add_edge('a', 'f')
+    graph_two.del_node('f')
+    assert 'f' not in graph_two._graph['a']
+
+
+def test_del_node_in_multi_val(graph_three):
+    """Test node is not in any value."""
+    graph_three.add_edge('a', 'z')
+    graph_three.add_edge('g', 'z')
+    graph_three.add_edge('u', 'z')
+    graph_three.del_node('z')
+    result = []
+    for key in graph_three._graph:
+        val = graph_three._graph[key]
+        result.extend(val)
+    assert result == []
